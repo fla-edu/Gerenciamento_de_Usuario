0acc00e20ab980938e6fd3ec7c30837232c2fda2
Flavio <fla_edu@live.com>
2020-01-05 18:01:46 -0300

    Manipulação de Arquivos

---
+++ controllers/UserController.js
@@ -0,0 +1,107 @@
+class UserController {
+
+    constructor(formID, tableID){
+
+        this.formEl = document.getElementById(formID);
+        this.tableEl = document.getElementById(tableID);
+
+        this.onSubmit();
+    }
+
+    getValues(){
+
+        let user = {};
+
+        [...this.formEl.elements].forEach((field, index)=>{
+    
+            if(field.name == 'gender' && field.checked){
+                user[field.name] = field.value;
+            }else if(field.name == 'admin'){
+                user[field.name] = field.checked;
+            }else{
+                user[field.name] = field.value;
+            }
+            
+        });
+    
+        return new User(
+            user.name, 
+            user.gender, 
+            user.birth,
+            user.country,
+            user.email,
+            user.password,
+            user.photo,
+            user.admin
+        );
+
+    }
+
+    onSubmit(){
+
+        this.formEl.addEventListener('submit', event =>{
+
+            event.preventDefault();
+
+            let values = this.getValues();
+
+            this.getPhoto().then(
+                (content) =>{
+                    values.photo = content;
+
+                    this.addLine(values);
+                },
+                (e) =>{
+                    console.error(e);
+                }
+            );
+
+        });
+
+    }
+
+    addLine(dataUser){
+
+        let tr = document.createElement('tr');
+        
+        tr.innerHTML = `
+            <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        `;
+        this.tableEl.appendChild(tr);
+    }
+
+    getPhoto(){
+
+        return new Promise((resolve, reject) =>{
+            let fileReader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item =>{
+                if (item.name === 'photo') return item;
+            })
+    
+            let file = elements[0].files[0];
+    
+            fileReader.onload = ()=>{
+                resolve(fileReader.result);
+            };
+    
+            fileReader.onerror = (e) =>{
+                reject(e);
+            };
+
+            if (file) fileReader.readAsDataURL(file);
+            else resolve('dist/img/boxed-bg.jpg');             
+            
+        });
+        
+    }
+
+}
