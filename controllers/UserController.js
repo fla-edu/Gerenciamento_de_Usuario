7e24f7d78269ac97e0818fcf1255e6b53cab1f03
Flavio <fla_edu@live.com>
2020-01-05 23:29:12 -0300

    Formatação de Data e Manipulação das Estatísticas

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -11,8 +11,14 @@ class UserController {
     getValues(){
 
         let user = {};
+        let isValid = true;
 
         [...this.formEl.elements].forEach((field, index)=>{
+
+            if(['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value){
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+            }
     
             if(field.name == 'gender' && field.checked){
                 user[field.name] = field.value;
@@ -24,6 +30,8 @@ class UserController {
             
         });
     
+        if(!isValid) return false;
+
         return new User(
             user.name, 
             user.gender, 
@@ -43,13 +51,22 @@ class UserController {
 
             event.preventDefault();
 
+            let btn = this.formEl.querySelector('[type=submit')
             let values = this.getValues();
+
+            if(!values) return false;
+
+            btn.disabled = true;
 
             this.getPhoto().then(
                 (content) =>{
                     values.photo = content;
 
                     this.addLine(values);
+
+                    this.formEl.reset();
+
+                    btn.disabled = false;
                 },
                 (e) =>{
                     console.error(e);
@@ -63,19 +80,23 @@ class UserController {
     addLine(dataUser){
 
         let tr = document.createElement('tr');
+
+        tr.dataset.user = JSON.stringify(dataUser);
         
         tr.innerHTML = `
             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
             <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
-            <td>${dataUser.birth}</td>
+            <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
         `;
         this.tableEl.appendChild(tr);
+
+        this.updateCount();
     }
 
     getPhoto(){
@@ -102,6 +123,25 @@ class UserController {
             
         });
         
+    }
+
+    updateCount(){
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+        [...this.tableEl.children].forEach(tr=>{
+            
+            numberUsers++;
+
+            let user = JSON.parse(tr.dataset.user);
+
+            if(user._admin) numberAdmin++;
+        });
+
+        document.querySelector('#number-users').innerHTML = numberUsers;
+        document.querySelector('#number-users-admin').innerHTML = numberAdmin;
+
     }
 
 }
